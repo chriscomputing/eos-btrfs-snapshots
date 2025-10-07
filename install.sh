1e81aa58f4c95b34481060a117bc31fc38886ac2
chriscomputing <189795337+chriscomputing@users.noreply.github.com>
2025-10-07 11:37:59 +0200

    Added install and uninstall script

---
+++ install.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# eos-snapshots install v1
+
+# Do NOT edit the first two lines
+# This script installs the necessary files and sets up the system for snapshots
+# We use sensible defaults. If you don't like them, you can easily change them, since all we use are a few bash scripts
+
+set -e
+
+# check if run as root, exit if not
+if [[ $EUID -ne 0 ]]; then
+    echo "$0 must be run as root."
+    exit 2
+fi
+
+# check if grub bootloader is used
+if ! grep -q "GRUB" /boot/grub/grub.cfg 2>/dev/null; then
+    echo "GRUB bootloader not detected. Exiting."
+    exit 2
+fi
+
+# check if grub-btrfs and inotify-tools are installed
+for pkg in grub-btrfs inotify-tools; do
+    if ! pacman -Q $pkg &>/dev/null; then
+        echo "$pkg is not installed. Please install it with: sudo pacman -S $pkg"
+        exit 2
+    fi
+done
+
+echo "Have you set up grub-btrfs to your liking? The default location for root snapshots is /.snapshots."
+echo "If you have changed this in the grub-btrfs config, you must also update systemd/eos-btrfs-rootsnapshot.service before continuing."
+read -p "Is grub-btrfs set up to your liking? (y/n): " answer
+if [[ "$answer" != "y" ]]; then
+    echo "Exiting. Please update your configuration as needed."
+    exit 2
+fi
+
+# make sure neither /.snapshots nor /home/.snapshots exist
+for dir in /.snapshots /home/.snapshots; do
+    if [ -e "$dir" ]; then
+        echo "Error: $dir already exists. Please remove or rename it before continuing."
+        exit 2
+    fi
+done
+
+# create /.snapshots and /home/.snapshots subvolumes
+btrfs subvolume create /.snapshots
+btrfs subvolume create /home/.snapshots
+
+# copy snapshot script
+install -m 700 src/create-snapshot.sh /usr/local/sbin/
+
+# copy systemd files
+install -m 644 systemd/* /etc/systemd/system
+
+# enable systemd units
+systemctl enable --now eos-btrfs-homesnapshot.timer
+systemctl enable --now eos-btrfs-rootsnapshot.timer
+
+echo "Installed eos-btrfs-snapshot successfully"
---
+++ uninstall.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# eos-snapshots uninstall v1
+
+# Do NOT edit the first two lines
+# Uninstall eos-btrfs-snapshots
+
+set -e
+
+# make sure script is run as root
+if [[ $EUID -ne 0 ]]; then
+    echo "$0 must be run as root."
+    exit 2
+fi
+
+# ask if user is sure they want to uninstall
+echo "This script will uninstall eos-btrfs-snapshot. It will NOT delete your snapshots"
+read -p "Continue? (y/n)" answer
+if [[ "$answer" != "y" ]]; then
+    echo "Exiting"
+    exit 2
+fi
+
+# disable timers
+systemctl disable --now eos-btrfs-homesnapshot.timer
+systemctl disable --now eos-btrfs-rootsnapshot.timer
+
+# remove create-snapshot script
+rm /usr/local/sbin/create-snapshot.sh
+
+# remove systemd timers and services
+rm /etc/systemd/system/eos-btrfs-homesnapshot.service
+rm /etc/systemd/system/eos-btrfs-homesnapshot.timer
+rm /etc/systemd/system/eos-btrfs-rootsnapshot.service
+rm /etc/systemd/system/eos-btrfs-rootsnapshot.timer
+
+# done
+echo "Successfully uninstalled eos-btrfs-snapshot"
